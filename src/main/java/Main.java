3e105209e5c1c739fdaef0a31ff59bd368ed0e72
AaronKibysz <aaronkibysz@gmail.com>
2024-09-23 08:55:12 -0300

    Commit segundo aaron

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
-import entities.Articulo;
-import entities.Cliente;
-import entities.Imagenes;
-import entities.UnidadMedida;
+import entities.*;
 import repositories.InMemoryRepository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +13,7 @@ public class Main {
         InMemoryRepository<UnidadMedida> unidadMedidaInMemoryRepository = new InMemoryRepository<>();
         InMemoryRepository<Articulo> articuloInMemoryRepository = new InMemoryRepository<>();
         InMemoryRepository<Imagenes> imagenesInMemoryRepository = new InMemoryRepository<>();
+        InMemoryRepository<Pedido> pedidoRepository = new InMemoryRepository<>();
 
         Cliente cliente1 = Cliente.builder()
                 .nombre("Carlos")
@@ -194,6 +195,187 @@ public class Main {
         articuloInMemoryRepository.save(cerveza1);
         articuloInMemoryRepository.save(cerveza2);
 
+        //DESARROLLO DE EJERCICIOS
 
+        double total = 0;
+        double totalCosto = 0;
+        DetallePedido pizzaC1 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(pizzaGrande2)
+                .build();
+        pizzaC1.setSubTotal(pizzaC1.getArticulo().getPrecioVenta() * pizzaC1.getCantidad());
+        DetallePedido cervezaC1 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(cerveza2)
+                .build();
+        cervezaC1.setSubTotal(cervezaC1.getArticulo().getPrecioVenta() * cervezaC1.getCantidad());
+        Pedido pedidoCliente1 = Pedido.builder()
+                .horaEstimadaFinalizacion(LocalTime.of(20, 45))
+                .estado(Estado.terminado)
+                .tipoEnvio(Envio.retiroLocal)
+                .formaDePago(FormaPago.debito)
+                .fechaPedido(LocalDate.now())
+                .build();
+        pedidoCliente1.getDetalles().add(pizzaC1);
+        pedidoCliente1.getDetalles().add(cervezaC1);
+        for (DetallePedido detallePedido : pedidoCliente1.getDetalles()) {
+            total += detallePedido.getSubTotal() * detallePedido.getCantidad();
+            totalCosto += detallePedido.getArticulo().getPrecioCompra() * detallePedido.getCantidad();
+        }
+        pedidoCliente1.setTotal(total);
+        pedidoCliente1.setTotalCosto(totalCosto);
+        total = 0;
+        totalCosto = 0;
+
+        DetallePedido pizzaC2 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(pizzaChica1)
+                .build();
+        pizzaC2.setSubTotal(pizzaC2.getArticulo().getPrecioVenta() * pizzaC2.getCantidad());
+        DetallePedido cervezaC2 = DetallePedido.builder()
+                .cantidad(3)
+                .articulo(cerveza2)
+                .build();
+        cervezaC2.setSubTotal(cervezaC2.getArticulo().getPrecioVenta() * cervezaC2.getCantidad());
+        Pedido pedidoCliente2 = Pedido.builder()
+                .horaEstimadaFinalizacion(LocalTime.of(21, 15))
+                .estado(Estado.terminado)
+                .tipoEnvio(Envio.delivery)
+                .formaDePago(FormaPago.credito)
+                .fechaPedido(LocalDate.now())
+                .build();
+        pedidoCliente2.getDetalles().add(pizzaC2);
+        pedidoCliente2.getDetalles().add(cervezaC2);
+        for (DetallePedido detallePedido : pedidoCliente2.getDetalles()) {
+            total += detallePedido.getSubTotal() * detallePedido.getCantidad();
+            totalCosto += detallePedido.getArticulo().getPrecioCompra() * detallePedido.getCantidad();
+        }
+        pedidoCliente2.setTotal(total);
+        pedidoCliente2.setTotalCosto(totalCosto);
+        total = 0;
+        totalCosto = 0;
+
+        DetallePedido pizza1C3 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(pizzaGrande1)
+                .build();
+        pizza1C3.setSubTotal(pizza1C3.getArticulo().getPrecioVenta() * pizza1C3.getCantidad());
+        DetallePedido pizza2C3 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(pizzaChica3)
+                .build();
+        pizza2C3.setSubTotal(pizza2C3.getArticulo().getPrecioVenta() * pizza2C3.getCantidad());
+        DetallePedido pizza3C3 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(pizzaGrande2)
+                .build();
+        pizza3C3.setSubTotal(pizza3C3.getArticulo().getPrecioVenta() * pizza3C3.getCantidad());
+        DetallePedido cervezaC3 = DetallePedido.builder()
+                .cantidad(2)
+                .articulo(cerveza1)
+                .build();
+        cervezaC3.setSubTotal(cervezaC3.getArticulo().getPrecioVenta() * cervezaC3.getCantidad());
+        Pedido pedidoCliente3 = Pedido.builder()
+                .horaEstimadaFinalizacion(LocalTime.of(22, 0))
+                .estado(Estado.terminado)
+                .tipoEnvio(Envio.retiroLocal)
+                .formaDePago(FormaPago.efectivo)
+                .fechaPedido(LocalDate.now())
+                .build();
+        pedidoCliente3.getDetalles().add(pizza1C3);
+        pedidoCliente3.getDetalles().add(pizza2C3);
+        pedidoCliente3.getDetalles().add(pizza3C3);
+        pedidoCliente3.getDetalles().add(cervezaC3);
+        for (DetallePedido detallePedido : pedidoCliente3.getDetalles()) {
+            total += detallePedido.getSubTotal() * detallePedido.getCantidad();
+            totalCosto += detallePedido.getArticulo().getPrecioCompra() * detallePedido.getCantidad();
+        }
+        pedidoCliente3.setTotal(total);
+        pedidoCliente3.setTotalCosto(totalCosto);
+
+        pedidoRepository.save(pedidoCliente1);
+        pedidoRepository.save(pedidoCliente2);
+        pedidoRepository.save(pedidoCliente3);
+
+        cliente1.getHace().add(pedidoCliente1);
+        cliente2.getHace().add(pedidoCliente2);
+        cliente3.getHace().add(pedidoCliente3);
+
+        System.out.println("--------BIENVENIDO--------\nPedido del cliente 1 llamado " + cliente1.getNombre());
+        Optional<Pedido> traerPedidoCliente1 = pedidoRepository.findById(pedidoCliente1.getId());
+        traerPedidoCliente1.ifPresent(pedido -> {
+            System.out.println("El cliente ordeno:");
+            for (DetallePedido detalle : pedido.getDetalles()) {
+                System.out.println(detalle.getCantidad() + " " + detalle.getArticulo().getDenominacion());
+            }
+            System.out.println("El cliente debe pagar $" + pedido.getTotal());
+        });
+
+        System.out.println("--------------------------------\nPedido del cliente 2 llamado " + cliente2.getNombre());
+        Optional<Pedido> traerPedidoCliente2 = pedidoRepository.findById(pedidoCliente2.getId());
+        traerPedidoCliente2.ifPresent(pedido -> {
+            System.out.println("El cliente ordeno:");
+            for (DetallePedido detalle : pedido.getDetalles()) {
+                System.out.println(detalle.getCantidad() + " " + detalle.getArticulo().getDenominacion());
+            }
+            System.out.println("El cliente debe pagar $" + pedido.getTotal());
+        });
+
+        System.out.println("--------------------------------\nPedido del cliente 3 llamado " + cliente3.getNombre());
+        Optional<Pedido> traerPedidoCliente3 = pedidoRepository.findById(pedidoCliente3.getId());
+        traerPedidoCliente3.ifPresent(pedido -> {
+            System.out.println("El cliente ordeno:");
+            for (DetallePedido detalle : pedido.getDetalles()) {
+                System.out.println(detalle.getCantidad() + " " + detalle.getArticulo().getDenominacion());
+            }
+            System.out.println("El cliente debe pagar $" + pedido.getTotal());
+        });
+
+        total = 0;
+        totalCosto = 0;
+        DetallePedido pizzaC1P2 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(pizzaChica3)
+                .build();
+        pizzaC1P2.setSubTotal(pizzaC1P2.getArticulo().getPrecioVenta() * pizzaC1P2.getCantidad());
+        DetallePedido cervezaC1P2 = DetallePedido.builder()
+                .cantidad(1)
+                .articulo(cerveza1)
+                .build();
+        cervezaC1P2.setSubTotal(cervezaC1P2.getArticulo().getPrecioVenta() * cervezaC1P2.getCantidad());
+        Pedido segundoPedidoCliente1 = Pedido.builder()
+                .horaEstimadaFinalizacion(LocalTime.of(23, 10))
+                .estado(Estado.preparacion)
+                .tipoEnvio(Envio.retiroLocal)
+                .formaDePago(FormaPago.efectivo)
+                .fechaPedido(LocalDate.now())
+                .build();
+        segundoPedidoCliente1.getDetalles().add(pizzaC1P2);
+        segundoPedidoCliente1.getDetalles().add(cervezaC1P2);
+        for (DetallePedido detallePedido : segundoPedidoCliente1.getDetalles()) {
+            total += detallePedido.getSubTotal() * detallePedido.getCantidad();
+            totalCosto += detallePedido.getArticulo().getPrecioCompra() * detallePedido.getCantidad();
+        }
+        segundoPedidoCliente1.setTotal(total);
+        segundoPedidoCliente1.setTotalCosto(totalCosto);
+
+        pedidoRepository.save(segundoPedidoCliente1);
+        cliente1.getHace().add(segundoPedidoCliente1);
+
+        System.out.println("--------------------------------\nSegundo pedido del cliente 1 llamado " + cliente1.getNombre());
+        Optional<Pedido> traerSegundoPedidoCliente1 = pedidoRepository.findById(segundoPedidoCliente1.getId());
+        traerSegundoPedidoCliente1.ifPresent(pedido -> {
+            System.out.println("El cliente ordeno:");
+            for (DetallePedido detalle : pedido.getDetalles()) {
+                System.out.println(detalle.getCantidad() + " " + detalle.getArticulo().getDenominacion());
+            }
+            System.out.println("El cliente debe pagar $" + pedido.getTotal());
+        });
+
+        System.out.println("--------------------------------\nEl cliente 3 llamado " + cliente1.getNombre() + " pidio " + cliente1.getHace().size() + " veces");
+
+        System.out.println("--------------------------------\nTODOS LOS ARTICULOS DEL NEGOCIO");
+        List<Articulo> todosArticulos = articuloInMemoryRepository.findAll();
+        todosArticulos.forEach(System.out::println);
     }
 }
